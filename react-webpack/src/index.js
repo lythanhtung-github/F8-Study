54d80e58f2773c51fe7a36a3a7ec9d77c23646cd
Lý Thanh Tùng <lythanhtung.work@gmail.com>
2022-12-09 15:20:05 +0700

    study

---
+++ react-webpack/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react' // nạp thư viện react
+import ReactDOM from 'react-dom/client' // nạp thư viện react-dom
+
+// Tạo component App
+function App() {
+    return (
+        <div>
+            <h1>Xin chào anh em F8!</h1>
+        </div>
+    )
+}
+
+// Render component App vào #root element
+// ReactDOM.render(<App />, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
